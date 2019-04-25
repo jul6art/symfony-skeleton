061851b0fd76dd916b68c3fe54895c19dc13e48a
jul6art <gkratz@interact.lu>
2019-04-25 13:44:42 +0200

    first crud

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,18 +2,95 @@
 
 namespace App\Controller;
 
+use App\Entity\Test;
+use App\Form\TestType;
+use App\Repository\TestRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/test")
+ */
 class TestController extends AbstractController
 {
     /**
-     * @Route("/test", name="test")
+     * @Route("/", name="test_index", methods={"GET"})
      */
-    public function index()
+    public function index(TestRepository $testRepository): Response
     {
         return $this->render('test/index.html.twig', [
-            'controller_name' => 'TestController',
+            'tests' => $testRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/new", name="test_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $test = new Test();
+        $form = $this->createForm(TestType::class, $test);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($test);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('test_index');
+        }
+
+        return $this->render('test/new.html.twig', [
+            'test' => $test,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="test_show", methods={"GET"})
+     */
+    public function show(Test $test): Response
+    {
+        return $this->render('test/show.html.twig', [
+            'test' => $test,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="test_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Test $test): Response
+    {
+        $form = $this->createForm(TestType::class, $test);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('test_index', [
+                'id' => $test->getId(),
+            ]);
+        }
+
+        return $this->render('test/edit.html.twig', [
+            'test' => $test,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="test_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Test $test): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$test->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($test);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('test_index');
     }
 }
