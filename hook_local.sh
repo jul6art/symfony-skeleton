d08f9b899536589de1817d8b2c61f97ae9c1dcfc
jul6art <gkratz@interact.lu>
2019-04-25 17:45:28 +0200

    webpack

--- hook_local.sh
+++ hook_local.sh
@@ -2,16 +2,17 @@
 
 set -e
 
+echo '--- FILES LOADING ---'
+yarn install
+yarn encore dev
+composer install --no-suggest
+php bin/console cache:clear
+php bin/console cache:warmup
+
 echo '--- DATABASE LOADING ---'
 php bin/console doctrine:schema:drop --force
 php bin/console doctrine:schema:update --force
 # php bin/console doctrine:fixtures:load --no-interaction
-
-echo '--- FILES LOADING ---'
-# yarn install
-composer install --no-suggest
-php bin/console cache:clear
-php bin/console cache:warmup
 
 # echo '--- TESTS LAUNCHING ---'
 # ./vendor/bin/simple-phpunit > tests_output.txt
--- hook_prod.sh
+++ hook_prod.sh
@@ -2,9 +2,12 @@ cd /home/symfony-skeleton/public_html/
 
 set -e
 
+echo '--- FILES LOADING ---'
+yarn install
+yarn encore production
 composer install --no-suggest
-
-php bin/console doctrine:migrations:migrate --no-interaction
-
 php bin/console cache:clear
 php bin/console cache:warmup
+
+echo '--- DATABASE LOADING ---'
+php bin/console doctrine:migrations:migrate --no-interaction
