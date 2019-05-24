1e89a23d60a84874ec8bd1020da77ac748f33abb
jul6art <gkratz@interact.lu>
2019-05-24 15:23:23 +0200

    tables

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,7 @@ Encore
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
+    .addEntry('table', './assets/js/table.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
