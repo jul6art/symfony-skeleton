4687070a227c84f9d4ede79a07358296283630f8
jul6art <gkratz@interact.lu>
2019-05-24 20:00:34 +0200

    tables

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,18 +1,225 @@
 // datatable
-require('../css/plugins/datatable/material.min.css');
-require('../css/plugins/datatable/datatable.min.css');
+require('../css/table.scss');
 require('datatables.net');
 require('datatables.net-dt');
 
-/*!
- DataTables Bootstrap 3 integration
- ©2011-2015 SpryMedia Ltd - datatables.net/license
-*/
-(function(c){"function"===typeof define&&define.amd?define(["jquery","datatables.net"],function(a){return c(a,window,document)}):"object"===typeof exports?module.exports=function(a,d){a||(a=window);if(!d||!d.fn.dataTable)d=require("datatables.net")(a,d).$;return c(d,a,a.document)}:c(jQuery,window,document)})(function(c,a,d,r){var g=c.fn.dataTable;c.extend(!0,g.defaults,{dom:"<'mdl-grid'<'mdl-cell mdl-cell--6-col'l><'mdl-cell mdl-cell--6-col'f>><'mdl-grid dt-table'<'mdl-cell mdl-cell--12-col'tr>><'mdl-grid'<'mdl-cell mdl-cell--4-col'i><'mdl-cell mdl-cell--8-col'p>>",
-    renderer:"material"});c.extend(g.ext.classes,{sWrapper:"dataTables_wrapper form-inline dt-material",sFilterInput:"form-control input-sm",sLengthSelect:"form-control input-sm",sProcessing:"dataTables_processing panel panel-default"});g.ext.renderer.pageButton.material=function(a,h,s,t,i,n){var o=new g.Api(a),l=a.oLanguage.oPaginate,u=a.oLanguage.oAria.paginate||{},f,e,p=0,q=function(d,g){var m,h,j,b,k=function(a){a.preventDefault();!c(a.currentTarget).hasClass("disabled")&&o.page()!=a.data.action&&
-o.page(a.data.action).draw("page")};m=0;for(h=g.length;m<h;m++)if(b=g[m],c.isArray(b))q(d,b);else{f="";j=!1;switch(b){case "ellipsis":f="&#x2026;";e="disabled";break;case "first":f=l.sFirst;e=b+(0<i?"":" disabled");break;case "previous":f=l.sPrevious;e=b+(0<i?"":" disabled");break;case "next":f=l.sNext;e=b+(i<n-1?"":" disabled");break;case "last":f=l.sLast;e=b+(i<n-1?"":" disabled");break;default:f=b+1,e="",j=i===b}j&&(e+=" mdl-button--raised mdl-button--colored");f&&(j=c("<button>",{"class":"mdl-button "+
-    e,id:0===s&&"string"===typeof b?a.sTableId+"_"+b:null,"aria-controls":a.sTableId,"aria-label":u[b],"data-dt-idx":p,tabindex:a.iTabIndex,disabled:-1!==e.indexOf("disabled")}).html(f).appendTo(d),a.oApi._fnBindAction(j,{action:b},k),p++)}},k;try{k=c(h).find(d.activeElement).data("dt-idx")}catch(v){}q(c(h).empty().html('<div class="pagination"/>').children(),t);k!==r&&c(h).find("[data-dt-idx="+k+"]").focus()};return g});
+/*! DataTables Bootstrap 3 integration
+ * ©2011-2014 SpryMedia Ltd - datatables.net/license
+ */
+
+/**
+ * DataTables integration for Bootstrap 3. This requires Bootstrap 3 and
+ * DataTables 1.10 or newer.
+ *
+ * This file sets the defaults and adds options to DataTables to style its
+ * controls using Bootstrap. See http://datatables.net/manual/styling/bootstrap
+ * for further information.
+ */
+(function(window, document, undefined){
+
+    var factory = function( $, DataTable ) {
+        "use strict";
+
+
+        /* Set the defaults for DataTables initialisation */
+        $.extend( true, DataTable.defaults, {
+            dom:
+            "<'row'<'col-sm-6'l><'col-sm-6'f>>" +
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row'<'col-sm-5'i><'col-sm-7'p>>",
+            renderer: 'bootstrap'
+        } );
+
+
+        /* Default class modification */
+        $.extend( DataTable.ext.classes, {
+            sWrapper:      "dataTables_wrapper form-inline dt-bootstrap",
+            sFilterInput:  "form-control input-sm",
+            sLengthSelect: "form-control input-sm"
+        } );
+
+
+        /* Bootstrap paging button renderer */
+        DataTable.ext.renderer.pageButton.bootstrap = function ( settings, host, idx, buttons, page, pages ) {
+            var api     = new DataTable.Api( settings );
+            var classes = settings.oClasses;
+            var lang    = settings.oLanguage.oPaginate;
+            var btnDisplay, btnClass, counter=0;
+
+            var attach = function( container, buttons ) {
+                var i, ien, node, button;
+                var clickHandler = function ( e ) {
+                    e.preventDefault();
+                    if ( !$(e.currentTarget).hasClass('disabled') ) {
+                        api.page( e.data.action ).draw( 'page' );
+                    }
+                };
+
+                for ( i=0, ien=buttons.length ; i<ien ; i++ ) {
+                    button = buttons[i];
+
+                    if ( $.isArray( button ) ) {
+                        attach( container, button );
+                    }
+                    else {
+                        btnDisplay = '';
+                        btnClass = '';
+
+                        switch ( button ) {
+                            case 'ellipsis':
+                                btnDisplay = '&hellip;';
+                                btnClass = 'disabled';
+                                break;
+
+                            case 'first':
+                                btnDisplay = lang.sFirst;
+                                btnClass = button + (page > 0 ?
+                                    '' : ' disabled');
+                                break;
+
+                            case 'previous':
+                                btnDisplay = lang.sPrevious;
+                                btnClass = button + (page > 0 ?
+                                    '' : ' disabled');
+                                break;
+
+                            case 'next':
+                                btnDisplay = lang.sNext;
+                                btnClass = button + (page < pages-1 ?
+                                    '' : ' disabled');
+                                break;
+
+                            case 'last':
+                                btnDisplay = lang.sLast;
+                                btnClass = button + (page < pages-1 ?
+                                    '' : ' disabled');
+                                break;
+
+                            default:
+                                btnDisplay = button + 1;
+                                btnClass = page === button ?
+                                    'active' : '';
+                                break;
+                        }
+
+                        if ( btnDisplay ) {
+                            node = $('<li>', {
+                                'class': classes.sPageButton+' '+btnClass,
+                                'id': idx === 0 && typeof button === 'string' ?
+                                    settings.sTableId +'_'+ button :
+                                    null
+                            } )
+                                .append( $('<a>', {
+                                        'href': '#',
+                                        'aria-controls': settings.sTableId,
+                                        'data-dt-idx': counter,
+                                        'tabindex': settings.iTabIndex
+                                    } )
+                                        .html( btnDisplay )
+                                )
+                                .appendTo( container );
+
+                            settings.oApi._fnBindAction(
+                                node, {action: button}, clickHandler
+                            );
+
+                            counter++;
+                        }
+                    }
+                }
+            };
+
+            // IE9 throws an 'unknown error' if document.activeElement is used
+            // inside an iframe or frame.
+            var activeEl;
+
+            try {
+                // Because this approach is destroying and recreating the paging
+                // elements, focus is lost on the select button which is bad for
+                // accessibility. So we want to restore focus once the draw has
+                // completed
+                activeEl = $(host).find(document.activeElement).data('dt-idx');
+            }
+            catch (e) {}
+
+            attach(
+                $(host).empty().html('<ul class="pagination"/>').children('ul'),
+                buttons
+            );
+
+            if ( activeEl ) {
+                $(host).find( '[data-dt-idx='+activeEl+']' ).focus();
+            }
+        };
+
+
+        /*
+         * TableTools Bootstrap compatibility
+         * Required TableTools 2.1+
+         */
+        if ( DataTable.TableTools ) {
+            // Set the classes that TableTools uses to something suitable for Bootstrap
+            $.extend( true, DataTable.TableTools.classes, {
+                "container": "DTTT btn-group",
+                "buttons": {
+                    "normal": "btn btn-default",
+                    "disabled": "disabled"
+                },
+                "collection": {
+                    "container": "DTTT_dropdown dropdown-menu",
+                    "buttons": {
+                        "normal": "",
+                        "disabled": "disabled"
+                    }
+                },
+                "print": {
+                    "info": "DTTT_print_info"
+                },
+                "select": {
+                    "row": "active"
+                }
+            } );
+
+            // Have the collection use a bootstrap compatible drop down
+            $.extend( true, DataTable.TableTools.DEFAULTS.oTags, {
+                "collection": {
+                    "container": "ul",
+                    "button": "li",
+                    "liner": "a"
+                }
+            } );
+        }
+
+    }; // /factory
+
+
+// Define as an AMD module if possible
+factory(jQuery, jQuery.fn.dataTable);
+
+
+})(window, document);
 
 $(document).ready(function() {
-    $('table.dataTable').DataTable();
+    var tables = $('table.dataTable');
+
+    tables.each(function () {
+        var datatableOptions = {
+            dom: 'Bfrtip',
+            language: DATATABLE_TRANSLATIONS,
+        };
+
+        if ($(this).data('export')) {
+            require('datatables.net-buttons');
+            datatableOptions.buttons = [
+                'copy', 'csv', 'excel', 'pdf'
+            ];
+        }
+
+        if ($(this).data('responsive')) {
+            require('datatables.net-responsive');
+            datatableOptions.responsive = true;
+        }
+
+        $(this).DataTable(datatableOptions);
+    });
 } );
