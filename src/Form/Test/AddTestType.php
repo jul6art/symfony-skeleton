1c808fd1f2392fb0c33ac87bb33bd19766c02ee2
jul6art <gkratz@interact.lu>
2019-06-10 13:18:39 +0200

    phpcs

--- src/Form/Test/AddTestType.php
+++ src/Form/Test/AddTestType.php
@@ -15,30 +15,30 @@ class AddTestType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-		$builder
-		    ->add('name', TextType::class, [
-		    	'label' => 'form.test.name.label',
-		        'attr' => [
-		            'data-inputmask' => "'mask': '99-9999999'",
-		            'pattern' => '\d{2}[\-]\d{7}',
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'form.test.name.label',
+                'attr' => [
+                    'data-inputmask' => "'mask': '99-9999999'",
+                    'pattern' => '\d{2}[\-]\d{7}',
 
-			        'data-toggle' => 'tooltip',
-			        'data-original-title' => 'Je ne suis pas encore traduit',
+                    'data-toggle' => 'tooltip',
+                    'data-original-title' => 'Je ne suis pas encore traduit',
 
-			        'data-alert' => 'form.test.name.help',
-			        'data-alert-class' => 'info',
-		        ],
+                    'data-alert' => 'form.test.name.help',
+                    'data-alert-class' => 'info',
+                ],
 
-			    'help' => 'form.test.name.help',
-		    ])
-		;
+                'help' => 'form.test.name.help',
+            ])
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => Test::class,
-	        'translation_domain' => 'form',
+            'translation_domain' => 'form',
         ]);
     }
 }
