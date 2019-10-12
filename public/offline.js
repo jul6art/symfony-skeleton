cde6f7a30d469be34ce3b4ac0213b97fe803c674
jul6art <gkratz@interact.lu>
2019-10-12 12:59:17 +0200

    pwa

--- public/offline.js
+++ public/offline.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'my-site-cache-v1';
+var CACHE_NAME = 'skeleton-cache-v1';
 var urlsToCache = [];
 
 self.addEventListener('install', function(event) {
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,26 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/offline.js').then(function(registration) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/offline.js').then(function (registration) {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(err) {
+        }, function (err) {
             // registration failed :(
             console.log('ServiceWorker registration failed: ', err);
         });
     });
 }
+
+// // Detects if device is on iOS
+// var isIos = function () {
+//     const userAgent = window.navigator.userAgent.toLowerCase();
+//     return /iphone|ipad|ipod/.test( userAgent );
+// };
+// // Detects if device is in standalone mode
+// var isInStandaloneMode = function () {
+//     return ('standalone' in window.navigator) && (window.navigator.standalone);
+// };
+//
+// // Checks if should display install popup notification:
+// if (isIos() && !isInStandaloneMode()) {
+//     var setState = { showInstallMessage: true };
+// }
